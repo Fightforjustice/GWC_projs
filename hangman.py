a1b1a6e9f8c87805ffeed3be4dc79e89b90da3ad
Fightforjustice <Fightforjustice@users.noreply.github.com>
2018-06-27 17:52:44 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -25,9 +25,9 @@ while (numfails < maxfails):
 	guess = input("Guess a letter:")
 	if (guess.isalpha() == False):
     		print("Not a letter")
-  		elif (guess not in word):
-    			print "Sorry; that's not one of the letters!"
-    			numfails+=1
+  	elif (guess not in word):
+    		print "Sorry; that's not one of the letters!"
+    		numfails+=1
   	else:
     		for i in word:
       			if (i == guess):
